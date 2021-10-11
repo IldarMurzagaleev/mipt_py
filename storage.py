4b5972e32af6202610e2c0f27dfb025424edd5e0
IldarMurzagaleev <send2mybox@mail.ru>
2021-10-11 15:11:32 +0500

    Create storage.py

---
+++ storage.py
@@ -0,0 +1,33 @@
+import argparse
+import os
+import tempfile
+import json
+from json.decoder import JSONDecodeError
+
+storage_path = os.path.join(tempfile.gettempdir(), 'storage6.data')
+parser = argparse.ArgumentParser()
+parser.add_argument("--key")
+parser.add_argument("--value")
+args = parser.parse_args()
+
+value = args.value
+key = args.key
+
+with open(storage_path, 'r') as fr:
+    try:
+        key_value_storage = json.load(fr)
+    except JSONDecodeError:
+        key_value_storage = {}
+
+if value is not None and key is not None:
+    if key not in key_value_storage:
+        key_value_storage[key] = [value]
+    else:
+        key_value_storage[key].append(value)
+    with open(storage_path, 'w') as fw:
+        json.dump(key_value_storage, fw)
+elif key is not None:
+    if key in key_value_storage.keys():
+        print(', '.join(key_value_storage[key]))
+    else:
+        print('Key', key, 'is not exist in storage')
