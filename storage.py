9740333e3e0d247ec0be97c54fd8129b77c9f2b2
IldarMurzagaleev <send2mybox@mail.ru>
2021-10-11 15:59:14 +0500

    Update storage.py

--- storage.py
+++ storage.py
@@ -5,7 +5,7 @@ import json
 from json.decoder import JSONDecodeError
 
 
-storage_path = os.path.join(tempfile.gettempdir(), 'storage7.data')
+storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
 parser = argparse.ArgumentParser()
 parser.add_argument("--key")
 parser.add_argument("--value")
@@ -14,23 +14,23 @@ args = parser.parse_args()
 value = args.value
 key = args.key
 
-with open(storage_path, 'w+') as fr:
-    try:
-        key_value_storage = json.load(fr)
-    except (FileNotFoundError, IOError):
-        key_value_storage = {}
-    except JSONDecodeError:
-        key_value_storage = {}
+try:
+    fr = open(storage_path, 'r')
+    key_value_storage = json.load(fr)
+except OSError:
+    key_value_storage = {}
+except JSONDecodeError:
+    key_value_storage = {}
 
 if value is not None and key is not None:
     if key not in key_value_storage:
         key_value_storage[key] = [value]
     else:
         key_value_storage[key].append(value)
-    with open(storage_path, 'w+') as fw:
+    with open(storage_path, 'w') as fw:
         json.dump(key_value_storage, fw)
 elif key is not None:
     if key in key_value_storage.keys():
         print(', '.join(key_value_storage[key]))
     else:
-        print('Key', key, 'is not exist in storage')
+        print(' ')
